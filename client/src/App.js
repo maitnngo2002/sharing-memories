f5edcc6bdd7151d5c4d77ed9168e4f298d1c6b9d
maingo <ngothingocmaiclht@gmail.com>
2021-08-26 20:23:39 -0400

    Refactor App component ( route)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 
-import Auth from './components/Auth/Auth';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Auth from "./components/Auth/Auth";
+
+import PostDetails from "./components/PostDetails/PostDetails";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/auth" exact component={Auth}/>
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route path="/posts/:id" exact component={PostDetails} />
+
+          <Route
+            path="/auth"
+            exact
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
         </Switch>
       </Container>
     </BrowserRouter>
-    
   );
 };
 
